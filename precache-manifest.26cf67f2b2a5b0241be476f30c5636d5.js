fc3fccdb8aa69315fb04311ae60beff942045af4
farshid manoochehri <farshid.manoochehri.k@gmail.com>
2020-03-20 01:36:30 +0330

    Updates

---
+++ precache-manifest.26cf67f2b2a5b0241be476f30c5636d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82de5071adbeb58e1ae248a73890576",
+    "url": "/farshid/index.html"
+  },
+  {
+    "revision": "3f99b91b927e395462ce",
+    "url": "/farshid/static/css/2.2c680a76.chunk.css"
+  },
+  {
+    "revision": "bcf539bf654ea77be65b",
+    "url": "/farshid/static/css/main.039713ef.chunk.css"
+  },
+  {
+    "revision": "3f99b91b927e395462ce",
+    "url": "/farshid/static/js/2.088b66b4.chunk.js"
+  },
+  {
+    "revision": "bcf539bf654ea77be65b",
+    "url": "/farshid/static/js/main.55917d90.chunk.js"
+  },
+  {
+    "revision": "c3f992d403aa5241e219",
+    "url": "/farshid/static/js/runtime~main.e7b927f2.js"
+  },
+  {
+    "revision": "e8354ea8e6c1432563271acb2f80cc30",
+    "url": "/farshid/static/media/dastnevis.e8354ea8.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/farshid/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/farshid/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/farshid/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/farshid/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/farshid/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "459169d74d4ff33f9d61d0d5d52f7b31",
+    "url": "/farshid/static/media/learning.459169d7.png"
+  },
+  {
+    "revision": "76ff687c325f1b7f4ff0fbaca3db9425",
+    "url": "/farshid/static/media/profile.76ff687c.jpg"
+  }
+]);
